98c63d945702cc642ff9bff857b439ee9f7f3357
meleciosilva <mpsilva94@gmail.com>
2021-04-09 13:18:35 -0400

    add getExistingNotes & getNoteId functions

---
+++ index.js
@@ -0,0 +1,14 @@
+
+function getExistingNotes() {
+  let notes = localStorage.getItem("notes");
+  if (!notes) return null;
+  return JSON.parse(notes);
+}
+
+function getNoteId() {
+  let noteObject = getExistingNotes();
+  if (!noteObject) return 1;
+  const keysArray = Object.keys(noteObject);
+  const numberKeys = keysArray.map(key => Number(key));
+  return Math.max(...numberKeys) + 1;
+}
